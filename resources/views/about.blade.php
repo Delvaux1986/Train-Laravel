d7ee76ab7a5b08adaa6f8fed85d7c3a943fb3634
Delvaux <Robby>
2020-10-20 16:47:57 +0200

    end of the day

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,19 +1,12 @@
 @include("include/header")
-    <section class='progressSection container-md mt-5 mb-5 col-md-8 offset-md-2 rounded' id='progress_certif'>
-        <h2 class="text-center pt-5"><ins>Mes progress</ins></h2>
-            <section class="text-center mt-5 mb-5 pb-5">
-                <label for='HTMLs' class="text-center">HTML :<progress min="0" max="100" value="100" id='HTMLs' class="progress-bar bg-info"></progress></label><br>
-                <label for='CSS3' class="text-center">CSS3 :<progress min="0" max="100" value="100" id='CSS3' class=" progress-bar bg-info"></progress></label><br>
-                <label for='PHP' class="text-center">PHP :<progress min="0" max="100" value="100" id='PHP' class="progress-bar bg-info"></progress></label><br>
-                <label for='Js' class="text-center">JavaScript :<progress min="0" max="100" value="100" id='Js' class=" progress-bar bg-info"></progress></label><br>
-    </section>
-    <section class="text-center flex-column" id="bgred">
+<div class="col-md-8 offset-md-2 bg-light rounded">
+    <section class="text-center flex-column mt-5 mb-5 col-md-8 offset-md-2 rounded bl-light">
             <h2><span><ins>Mes heures d'apprentissages</ins></span></h2>
-            <table border="2" class="text-center table table-active mt-4">
+            <table border="2" class="mb-4 text-center table table-active mt-4 pb-5">
                 <tr>
                     <th class="bg-primary">Jours</th>
                     <th class="bg-info">lun(présentiel)</th>
-                    <th class="bg-warning">mar(présentiel)</th>
+                    <th class="bg-warning">mar(remote)</th>
                     <th class="bg-info">mer(remote)</th>
                     <th class="bg-warning">jeu(remote)</th>
                     <th class="bg-info">ven(remote)</th>
@@ -27,9 +20,11 @@
                     <td class="bg-info">8h</td>
                 </tr>
             </table>
-        </section>
-
-
+    </section>
+    <section>
+        <p class="text-center">Ici Viendrons mes Badges ect.... </p>
+    </section>
+</div>
 
 
  @include("include/footer")
--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -1,3 +1,3 @@
-<footer class="fixed bottom">
+<footer class="fixed bottom text-center txt-info">
     Copyright2020 : Delvaux Robby
 </footer>
