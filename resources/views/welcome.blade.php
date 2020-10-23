0d82c18446d7fcbbc87c764fcaccaba6cbee7e3b
Delvaux <Robby>
2020-10-23 11:38:22 +0200

    try to fix bootstrap7

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <body class="bg-dark">
     <article class="d-flex justify-content-center flex-column container-md col-md-8 offset-md-2 bg-light rounded">
                 <h2 class="mt-5 text-info text-center">Delvaux Robby</h2>
-                    <img src="./img/moi.jpg" alt="Photo de moi" class="img-fluid text-center rounded"><br><br>
+                    <img src="./img/moi.jpg" alt="Photo de moi" class="text-center rounded"><br><br>
                         <p class="mb-5"> Bonjour , <br> 
                             Je m'apelle Robby Delvaux . <br>
                             Je suis actuellement en formation a <a href="https://becode.org/">Becode.org</a> . Ou j'ai déja vu jusqu'a présent 
