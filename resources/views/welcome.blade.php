e8ad753e18a7a49a0ae4b777faa94f8c8f8f768e
Delvaux <Robby>
2020-10-23 11:49:48 +0200

    try to fix bootstrap normaly its good

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,9 @@
     
     
     <body class="bg-dark">
-    <article class="d-flex justify-content-center flex-column container-md col-md-8 offset-md-2 bg-light rounded">
-                <h2 class="mt-5 text-info text-center">Delvaux Robby</h2>
-                    <img src="./img/moi.jpg" alt="Photo de moi" class="text-center rounded"><br><br>
+    <article class="firstcadre container-md">
+                <h2 class="mt-5">Delvaux Robby</h2>
+                    <img src="./img/moi.jpg" alt="Photo de moi" class="img-fluid"><br><br>
                         <p class="mb-5"> Bonjour , <br> 
                             Je m'apelle Robby Delvaux . <br>
                             Je suis actuellement en formation a <a href="https://becode.org/">Becode.org</a> . Ou j'ai déja vu jusqu'a présent 
@@ -15,16 +15,17 @@
                             J'ai commencer se site il n'est donc pas parfait mais je compte le tenir a jours et y ajouter les projets sur les quels 
                             je travail . Vous trouverez ici en dessous les liens vers Github , linkedin </p>
         </article>
-            <section class="container-md mb-5 mt-5 col-md-8 offset-md-2 bg-light rounded d-flex justify-content-center flex-row">
+            <section class="ReseauSociaux container-md mb-5 mt-5">
                 <ul>
-                    <li class="mt-5 mb-5 i-black">
-                        <a href="https://www.linkedin.com/in/robby-delvaux-1ab1a51ab/" target="_blank" class="text-dark">
-                        <i class="fab fa-linkedin fa-3x text-dark"></i></br/>Linkedin</a>
-                    </li>
-                        <a href="https://github.com/Delvaux1986" target="_blank" class="text-dark">
-                    <li class="mt-5 mb-5  "><i class="fab fa-github fa-3x text-dark"></i><br/>Github</a>
-                    </li>
+                        <li class="mt-5 mb-5 i-black">
+                            <a href="https://www.linkedin.com/in/robby-delvaux-1ab1a51ab/" target="_blank" class="text-dark">
+                            <i class="fab fa-linkedin fa-3x text-dark"></i></br/>Linkedin</a>
+                        </li>
+                            <a href="https://github.com/Delvaux1986?tab=repositories" target="_blank" class="text-dark">
+                        <li class="mt-5 mb-5  "><i class="fab fa-github fa-3x text-dark"></i><br/>Github</a>
+                        </li>
                 </ul>
+
             </section>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
