b05ab4e83da20299c22a033907e5f4d1fe3b48c6
Delvaux <Robby>
2020-10-23 11:01:06 +0200

    try to fix this Bootstrap first3

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -5,8 +5,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <link rel="stylesheet" href={{asset('css/style.css')}}>
-        <link rel="stylesheet" type="text/css" href={{ asset('/css/bootstrap.min.css') }}>
+        <link rel="stylesheet" href={{url::asset('css/style.css')}}>
+        <link rel="stylesheet" type="text/css" href={{ url::asset('/css/bootstrap.min.css') }}>
         <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.css">
         <title>Delvaux Robby</title>
